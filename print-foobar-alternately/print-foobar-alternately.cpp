ed8d128a897fa567bf5a59c6d9137000c4980781
sklationd <sklationd@gmail.com>
2020-08-07 19:21:23 +0900

    [Concurrency] print-foobar-alternately

---
+++ print-foobar-alternately/print-foobar-alternately.cpp
@@ -0,0 +1,30 @@
+class FooBar {
+private:
+    int n;
+    mutex m1;
+    mutex m2;
+
+public:
+    FooBar(int n) {
+        this->n = n;
+        m2.lock();
+    }
+
+    void foo(function<void()> printFoo) {
+        
+        for (int i = 0; i < n; i++) {
+            m1.lock();
+        	printFoo();
+            m2.unlock();
+        }
+    }
+
+    void bar(function<void()> printBar) {
+        
+        for (int i = 0; i < n; i++) {
+            m2.lock();
+        	printBar();
+            m1.unlock();
+        }
+    }
+};
