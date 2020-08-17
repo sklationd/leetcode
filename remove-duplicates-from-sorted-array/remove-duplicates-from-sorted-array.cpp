fb2a4272b8c1d6f4e2815fc061f1c05771cb2588
sklationd <sklationd@gmail.com>
2020-08-17 20:36:45 +0900

    [Set] remove-duplicates-from-sorted-array

--- remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
+++ remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -1,16 +1,9 @@
 class Solution {
 public:
-    int removeDuplicates(vector<int>& nums) {
-        unordered_set<int> S;
-        for(int i=0;i<nums.size();i++){
-            unordered_set<int>::const_iterator it=S.find(nums[i]);
-            if(it != S.end()){
-                nums.erase(nums.begin()+i);
-                i--;                
-            }
-            else
-                S.insert(nums[i]);
-        }
-        return S.size();
+    int removeDuplicates(vector<int>& nums)
+    {
+	    nums.erase(unique(nums.begin() ,nums.end()),nums.end() ); 
+	    return nums.size() ;
     }
+   
 };
